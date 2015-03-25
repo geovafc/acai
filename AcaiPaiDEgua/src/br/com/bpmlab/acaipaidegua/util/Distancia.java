cbc4727fa06ab5940740fceeb8dde6a755c07c8f
geovafc <geovafc@yahoo.com.br>
2015-03-25 04:05:06 -0300

    Logo

---
+++ AcaiPaiDEgua/src/br/com/bpmlab/acaipaidegua/util/Distancia.java
@@ -0,0 +1,32 @@
+package br.com.bpmlab.acaipaidegua.util;
+
+public class Distancia<T> implements Comparable<Distancia<T>> {
+
+    private final T t;
+    private final Integer distancia;
+
+    public Distancia(T t,
+            Integer distancia) {
+        this.t = t;
+        this.distancia = distancia;
+    }
+
+    @Override
+    public int compareTo(Distancia<T> o) {
+        if (o == null) {
+            return -1;
+        } else {
+            return distancia.compareTo(o.distancia);
+        }
+    }
+
+    public T getObjeto() {
+        return t;
+    }
+
+    @Override
+    public String toString() {
+        return t + " - " + distancia;
+    }
+
+}
---
+++ AcaiPaiDEgua/src/br/com/bpmlab/acaipaidegua/util/LatLonUtil.java
@@ -0,0 +1,7 @@
+package br.com.bpmlab.acaipaidegua.util;
+
+public class LatLonUtil {
+	public static double latUsuario;
+	public static double lonUsuario;
+
+}
