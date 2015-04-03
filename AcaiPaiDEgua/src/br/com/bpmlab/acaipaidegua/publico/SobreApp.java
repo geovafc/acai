774f6fe63bc6878fea14fb734b78620bc9b87c58
geovafc <geovafc@yahoo.com.br>
2015-04-03 18:22:13 -0300

    Layout Listar

--- AcaiPaiDEgua/src/br/com/bpmlab/acaipaidegua/publico/SobreApp.java
+++ AcaiPaiDEgua/src/br/com/bpmlab/acaipaidegua/publico/SobreApp.java
@@ -5,22 +5,20 @@ import android.app.Activity;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.text.Html;
-import android.view.View;
 import android.widget.TextView;
 
 public class SobreApp extends Activity {
 
-	
-	private TextView textoapp;
+	private TextView texto;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.sobreapp);
-		
-		textoapp = (TextView) findViewById(R.id.textoapp);
+
+		texto = (TextView) this.findViewById(R.id.textoapp);
 		Resources res = this.getResources();
-		textoapp.setText(Html.fromHtml(res.getString(R.string.texto)));
+		texto.setText(Html.fromHtml(res.getString(R.string.texto)));
 	}
-	
+
 }
